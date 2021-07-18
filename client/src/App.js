578092527aed58e322a6442b42d7353528e8172b
Abhishek Parashar <abhishekparashar@Abhisheks-MacBook-Pro.local>
2021-07-18 20:42:04 +0530

    navbar implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import{BrowserRouter,Switch,Route} from 'react-router-dom'
+import Home from './booking/Home'
+import Login from "./auth/Login"
+import Register from "./auth/Register"
+import NavTab from './components/NavTab'
 
 function App() {
   return (
     <div className="App">
-      HoTel Dekho
+      <BrowserRouter>
+      <NavTab/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/register" component={Register}/>
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
